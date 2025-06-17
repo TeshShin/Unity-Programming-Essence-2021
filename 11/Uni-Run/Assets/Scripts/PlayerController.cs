0d71bd83dc6a4f915bdaa30a3ff9688c5f938699
TeshShin <shintom1222@gmail.com>
2025-06-17 14:34:34 +0900

    유니런 완료

--- 11/Uni-Run/Assets/Scripts/PlayerController.cs
+++ 11/Uni-Run/Assets/Scripts/PlayerController.cs
@@ -25,31 +25,57 @@ public class PlayerController : MonoBehaviour {
         // 사용자 입력을 감지하고 점프하는 처리
         if (isDead) return;
 
-        if(Input.GetMouseButton(0) && jumpCount < jumpMax)
+        if(Input.GetMouseButtonDown(0) && jumpCount < jumpMax)
         {
+            Debug.Log(jumpCount);
             // 점프 횟수 증가
             jumpCount++;
             // 점프 직전에 속도를 순간적으로 제로로 변경
             playerRigidbody.velocity = Vector2.zero;
             // 리지드바디에 위쪽으로 힘 주기
             playerRigidbody.AddForce(new Vector2(0, jumpForce));
-
+            // 오디오 재생
+            playerAudio.Play();
         }
+        else if(Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)
+        {
+            // 마우스 왼쪽 버튼에서 손을 떼는 순간 && 속도의 y값이 양수라면(위로 상승 중)
+            // 현재 속도를 절반으로 변경
+            playerRigidbody.velocity = playerRigidbody.velocity * 0.5f;
+        }
+        animator.SetBool("Grounded", isGrounded);
     }
 
     private void Die() {
-        // 사망 처리
+        // 애니메이터의 Die 트리거
+        animator.SetTrigger("Die");
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+        playerRigidbody.velocity = Vector2.zero;
+        isDead = true;
+        GameManager.instance.OnPlayerDead();
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+        if(other.tag == "Dead" && !isDead)
+        {
+            Die();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         // 바닥에 닿았음을 감지하는 처리
+        // 어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있다면
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision) {
         // 바닥에서 벗어났음을 감지하는 처리
+        isGrounded = false;
     }
 }
